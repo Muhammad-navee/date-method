14e2f080c4bb644c03b48cb2d25b6b9c1375c52b
muhammad naveed <amirprinceak5@gmail.com>
2022-06-11 18:56:06 +0500

    complete

---
+++ app.js
@@ -0,0 +1,78 @@
+// Q no 1
+
+// var now = new Date();
+// document.write(now);
+
+// Q no 2
+
+// var monthNames = ["January", "February", "	March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// var now = new Date().getMonth();
+//  console.log(now);
+//  console.log("Current Month: " + " " + monthNames[now])
+
+
+// Q no 3
+
+// var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var now = new Date().getDay();
+//  console.log(now);
+//  console.log("Today is :" + " " + dayNames[now])
+
+// Q no 4
+
+//  var dayNames = ["Fun", "Mon", "Tue", "Wed", "Thu", "Fri", "fun"];
+//  var now = new Date().getDay();
+//   console.log(now);
+//   console.log("It's :" + " " + dayNames[now] + "day")
+
+// Q no 5
+
+// var d = new Date();
+// d.setDate(15)
+// console.log(d)
+
+
+// Q no 6 
+
+// var today = new Date();
+// console.log(today)
+
+// var doomsday =new .get("15 may 2022");
+// console.log(doomsday)
+
+             // OR    
+
+
+// Q no 7 
+
+// var time = new Date();
+// console.log(
+//     time.toLocaleDateString('en-US',{ hour: 'numeric', hour12: true})
+// );
+
+
+// Q no 8
+
+//var LaterDate = new Date("31 dec 2020 ");
+//console.log(LaterDate);
+
+//Q no 10
+
+
+// Q no 13 
+
+//  function calculate_age(dob) { 
+//     var diff_ms = Date.now() - dob.getTime();
+//     var age_dt = new Date(diff_ms); 
+  
+//     return Math.abs(age_dt.getUTCFullYear() - 1970);
+// }
+
+// console.log(calculate_age(new Date(2001, 5, 15)));
+
+
+// Q no 12
+
+// const currentYear = new Date().getFullYear();
+// const previousYear = currentYear-100;
+// console.log(previousYear );
